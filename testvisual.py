2a3b8f10d9739f9998d227c59a3eed19a7f6156b
Jonathan-Gore <43797248+Jonathan-Gore@users.noreply.github.com>
2020-04-24 17:10:38 -0600

    Reformatted SimplePyGui layout classes for less visual clutter

--- testvisual.py
+++ testvisual.py
@@ -4,15 +4,17 @@ import sys
 from subprocess import Popen
 import numpy as np
 
-layout3 = [ [sg.Text('Begin Visualizing Processing Results (turn fancy numbers [.json] into red squares on camera trap images)')],
-            [sg.Text('Backend Python Scripts'), sg.Input(key='-SCRIPTS_visual-'), sg.FileBrowse() ],
-            [sg.Text('Output JSON file created from previous step, most likely named: output.json, found in the same folder with your images'), sg.Input(key='-JSON_visual-'), sg.FileBrowse() ], ##changed sg.FolderBrowse to sg.FileBrowse
-            [sg.Text('Original Image Folder Location'), sg.Input(key='-DETECT_visual-'), sg.FolderBrowse() ],
-            [sg.Text('New Image Folder Location to Create'), sg.Input(key='-RENDER_visual-'), sg.FolderBrowse() ],
-            [sg.Text('Confidence Threshold for Detection [Scale of 0 - 1]'), sg.Input(key='-CONFIDENCE_visual-') ],
-            [sg.Button('Ok'), sg.Button('Cancel')]  ]
+layout = [[sg.Text('Begin Visualizing Processing Results (turn fancy numbers [.json] into red squares on camera trap images)')],
+          [sg.Text('Backend Python Scripts'), sg.Input(key='-SCRIPTS_visual-'), sg.FileBrowse() ],
+          [sg.Text('Output JSON file created from previous step, most likely named: output.json, found in the same folder with your images'), sg.Input(key='-JSON_visual-'), sg.FileBrowse() ], ##changed sg.FolderBrowse to sg.FileBrowse
+          [sg.Text('Original Image Folder Location'), sg.Input(key='-DETECT_visual-'), sg.FolderBrowse() ],
+          [sg.Text('New Image Folder Location to Create'), sg.Input(key='-RENDER_visual-'), sg.FolderBrowse() ],
+          [sg.Text('Confidence Threshold for Detection [Scale of 0 - 1]'), sg.Input(key='-CONFIDENCE_visual-') ],
+          [sg.Button('Ok'), sg.Button('Cancel')]]
 
-event, visualization = sg.Window('Window Title', layout3).read(close=True)
+layout2 = []
+
+event, visualization = sg.Window('Window Title', layout).read(close=True)
 
 if event == 'Ok':
     directory = 'C:\\git\\cameratrapsGUI\\bat'
